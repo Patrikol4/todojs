46a25c2ec49ac5e525654826bca48476688da585
Patrikol4 <doublecrt@gmail.com>
2025-12-26 21:42:29 -0300

    fixing layout

--- todos.js
+++ todos.js
@@ -6,16 +6,19 @@ var deleteBtnElement = document.querySelector('#app a')
 var todos = [
 	'Tomar café',
 	'Dar bom-dia pro meu gato',
-	];
+	'Aprender Javascript',
+	'Parar de usar IA de uma vez por todas'
+];
 
-function renderTodos(){
+function renderTodos() {
 
 	listElement.innerHTML = '';
 
-	for(todo of todos){
+	for (todo of todos) {
 		// ele cria o link da lista
 		var todoElement = document.createElement('li');
-		todoElement.classList.add('list-group-item')
+		todoElement.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start')
+		
 		var todoText = document.createTextNode(todo); // cria o texto da lista
 
 		var linkElement = document.createElement('button'); // cria o elemento do link do item
@@ -29,7 +32,7 @@ function renderTodos(){
 		var linkText = document.createTextNode(' Deletar Tarefa');
 
 		linkElement.appendChild(linkText);
-		
+
 		todoElement.appendChild(todoText);
 		todoElement.appendChild(linkElement);
 		listElement.appendChild(todoElement);
@@ -38,9 +41,12 @@ function renderTodos(){
 
 renderTodos();
 
-function addTodo(){
+function addTodo() {
 	var todoText = inputElement.value;
-
+	if (!inputElement.value) {
+		alert("Você precisa descrever sua tarefa.")
+		return console.log("Todo não criado devido a falta de descrição.")
+	}
 	todos.push(todoText);
 	todoText = inputElement.value = '';
 	renderTodos();
@@ -48,7 +54,7 @@ function addTodo(){
 
 buttonElement.onclick = addTodo;
 
-function deleteTodo(pos){
+function deleteTodo(pos) {
 
 	todos.splice(pos, 1);
 	renderTodos();
